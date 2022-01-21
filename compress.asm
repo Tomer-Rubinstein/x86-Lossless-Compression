38f18d0732cfc47c01ccf037123a6a7b7bd41dbe
TomeR <tomerrub11@gmail.com>
2022-01-21 21:53:48 +0200

    program can now take filename as user input

--- compress.asm
+++ compress.asm
@@ -4,13 +4,13 @@ STACK 100h
 
 ; TODO:
 ;	[compress]
-;	buildFreqArr
+;	buildFreqArr (DONE)
 ;	buildMinHeap
 ;	buildEncodedTable
 ;	outCompFile
 
 DATASEG
-	filename db 'file.txt', 0 ; [TODO] take filename as input
+	filename db ?
 	filename_len dw ?
 	filehandle dw ?
 	filecontent db 1200 dup(0)
@@ -18,17 +18,43 @@ DATASEG
 	freqArr dw 128 dup(0)
 
 	log_OpenError db 'Error: Program could not open the given file$'
+	log_InputFilename db 'File name (include extension): $' 
 
 CODESEG
 start:
 	mov ax, @data
 	mov ds, ax
 
+	call getFilename
 	call buildFreqArr
 
 exit:
 	mov ax, 4c00h
 	int 21h
+
+proc getFilename
+	; prompting the user to enter the filename
+	mov dx, offset log_InputFilename
+  mov ah, 9
+  int 21h
+
+	; getting user input until the newline char (13, in ASCII)
+	mov si, 0
+	loop_getFilename:
+		mov ah, 1
+		int 21h
+
+		cmp al, 13
+		je end_getFilename
+
+		mov [filename+si], al
+
+		inc si
+		jmp loop_getFilename
+
+	end_getFilename:
+	ret
+endp getFilename
 
 
 proc buildFreqArr
@@ -47,6 +73,8 @@ proc buildFreqArr
 	mov dx, offset filecontent
 	int 21h
 
+	; incrementing the element in freqArr corresponding to the current ASCII char value as index
+	; i.e. to increment the appearences of the char 'a', increment freqArr[97] by 1.
 	mov si, 0
 	loop_genFreqArr:
 		mov bx, 0
@@ -55,7 +83,7 @@ proc buildFreqArr
 		inc [freqArr+bx]
 
 		inc si
-		cmp [filecontent+si], 0
+		cmp [filecontent+si], 0 ; read until the null terminator
 		jne loop_genFreqArr
 
 	; close file
