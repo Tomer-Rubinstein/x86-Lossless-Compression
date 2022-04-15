fcd20fe45d66e06a75008881a2f1a9e8f58b042b
TomeR <tomerrub11@gmail.com>
2022-04-15 10:49:42 +0300

    Anna Zak test passed ✔️

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 # program to output the content of 'compress.hf' as binary
 
 with open('compress.hf', 'rb') as file:
+  byteCount = 0
   while True:
     try:
       byte = file.read(1)
       print('{0:08b}'.format(ord(byte)), end="\t")
       print(byte)
+      byteCount += 1
     except:
       break
   file.close()
+  print(f"\nByte count: {byteCount}")
